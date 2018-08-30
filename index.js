f0b733ab5d3a66ffeeaecbf14ac8c1f0cc2edd44
Alison Kanegae <alisonkanegae@gmail.com>
2018-08-30 00:51:18 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,29 +1,7 @@
 //1
-var katzDeli = [];
+var katzDeliLine = [];
 
 function takeANumber(katzDeli, name) {
   katzDeli.push(`${name}`);
-  return("Welcome, " + name + ". You are number " + katzDeli.length + " in line.");
-}
-
-//2
-function nowServing(katzDeli) {
-  var i = 0;
-  while ( i < katzDeli.length) {
-    i++;
-  }
-  if (katzDeli.length === 0) {
-    return "There is nobody waiting to be served!";
-  } else {
-    return (`Currently serving ${katzDeli.shift()}.`);
-  }
-}
-
-//3
-function currentLine(katzDeliLine) {
-  if (katzDeliLine.length === 0) {
-    return "The line is currently empty.";
-  } else {
-    return "The line is currently: 1. " + katzDeliLine[0] + ", 2. " + katzDeliLine[1] + ", 3. " + katzDeliLine[2];
-  }
+  return (`"Welcome, " + ${name} + ". You are number " + ${katzDeli.length} + " in line."`);
 }
